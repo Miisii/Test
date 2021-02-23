aeeaf9b50f65cdbc4d3ad45a32484fed500d7ebd
Miisii <79223686+Miisii@users.noreply.github.com>
2021-02-23 10:38:08 +0100

    Create CucumberRunnerTest.java

---
+++ CucumberRunnerTest.java
@@ -0,0 +1,16 @@
+package hu.idomsoft.divizio2.example.systemtest;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        strict = true,
+        features = "src/test/java/hu/idomsoft/divizio2/example/systemtest/",
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        plugin = {"progress", "json:target/executable-specification/cucumber.json"},
+        monochrome = true
+)
+public class CucumberRunnerTest {
+}
